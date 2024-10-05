89d4affcea5f2f0528c2009247daaf9f8550138d
Gnay <3215039870@qq.com>
2024-10-05 18:40:22 +0800

    feat: 修复文章详情渲染文字问题

--- src/views/detail/articalCompoment.vue
+++ src/views/detail/articalCompoment.vue
@@ -3,7 +3,7 @@
     <h1>{{ data.title }}</h1>
     <span>{{ data.nickname }}</span>
     <span>{{ time }}</span>
-    <p>{{ data.content }}</p>
+    <p v-html="data.content"></p>
   </div>
 </template>
 <script setup>
@@ -17,7 +17,6 @@ const getArticleDetail = async () => {
   const res = await artGetDetailService(route.params.id)
   data.value = res.data.data
   time.value = new Date(data.value.pub_date).toLocaleString()
-  data.value.content = data.value.content.replace(/<p>(.*?)<\/p>/g, '$1')
 }
 getArticleDetail()
 </script>
